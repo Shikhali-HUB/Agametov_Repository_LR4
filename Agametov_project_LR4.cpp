e83787d010ce53910588a584cd1ef38fe9a4f18a
Shikhali-HUB <shikhali01@mail.ru>
2025-04-08 22:29:04 +0300

    enter n

--- Agametov_project_LR4.cpp
+++ Agametov_project_LR4.cpp
@@ -6,14 +6,54 @@
 using namespace std;
 
 int N,X;
+bool UserInput(string input) {
+  if (input.empty())
+    return false;
+  try {
+    int number = stoi(input);
+  } catch (...) {
+    return false;
+  }
+  return true;
+}
+
+function<void()> EnterNumber(int &varLink, string label) {
+  return [&varLink, label]() {
+    string raw_input;
+    cout << label;
+    getline(cin, raw_input);
+
+    while (!UserInput(raw_input)) {
+      cout << label;
+      getline(cin, raw_input);
+    }
+    varLink = stoi(raw_input);
+  };
+}
+
 
 void enterX(){
-  EnterNumber(X,"Введите число x: ")();
-  cout << "X: " << X << endl;
+  while (true) {
+  EnterNumber(X,"Enter number x: ")();
+  if (X < 999 && X > 99) {
+    cout << "X: " << X << endl;
+    break;
+  } else{
+    cout << "Invalid number.\n";
+  }
+  }
 }
 
 void enterN(){
-
+  while (true) {
+  EnterNumber(N,"enter number n (less than X, i.e. 1 to 3): ")();
+  if (N >= 1 && N < 3) {
+    cout << "N:" << N <<endl;
+    break;
+  }else{
+    cout << "Invalid.\n";
+  }
+  }
 }
 
 void enterfirstX(){
@@ -23,30 +63,6 @@ void enterfirstX(){
 void enterNX(){
 
 }
-bool UserInput(string input) {
-    if (input.empty())
-      return false;
-    try {
-      int number = stoi(input);
-    } catch (...) {
-      return false;
-    }
-    return true;
-  }
-  
-  function<void()> EnterNumber(int &varLink, string label) {
-    return [&varLink, label]() {
-      string raw_input;
-      cout << label;
-      getline(cin, raw_input);
-  
-      while (!UserInput(raw_input)) {
-        cout << label;
-        getline(cin, raw_input);
-      }
-      varLink = stoi(raw_input);
-    };
-  }
 
 struct MenuItem{
         string title;
@@ -55,18 +71,18 @@ struct MenuItem{
 
 int main(){
     map<int,MenuItem>menu= {
-        {1,{"Ввод 1-ой стороны треугольника", EnterNumber(N,"Input side1")}},
-        {2,{"Ввод 2-ой стороны треугольника", EnterNumber(X,"Input side2")}},
+        {1,{"x",enterX}},
+        {2,{"n",enterN}},
     };
     int choice = 0;
 
     while (true) {
-        cout<<"Меню:"<<endl;
+        cout<<"Menu:"<<endl;
         for (const auto& item : menu) {
             cout << "Task " <<item.first <<". " << item.second.title <<endl;
         }
-        cout << "0. Выход" << endl;
-        EnterNumber(choice,"Введите номер пункта: ")();
+        cout << "0. Exit" << endl;
+        EnterNumber(choice,"Enter Task: ")();
         if (choice == 0 ){
             cout << "@ 2025 FirstName LastName" <<endl;
             break;
@@ -75,7 +91,7 @@ int main(){
         if(menu.find(choice)!= menu.end()){
             menu[choice].action();
         }else{
-            cout << "Некорректный ввод.";
+            cout << "Invalid input.";
         }
         cout << endl;
     }
